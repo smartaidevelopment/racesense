0c08491ad137306627105136e7b9feab87c1722d
smartaidevelopment <smartaidevelopment@gmail.com>
2025-06-29 22:27:47 +0300

    Ultra-simple App.tsx test

--- Desktop/Smart AI Development/RaceSense/src/App.tsx
+++ Desktop/Smart AI Development/RaceSense/src/App.tsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-const SimpleHome = () => {
-  return (RaceSenseProfessional Racing Telemetry Platform</pStart Racing SessionView Telemetry</buttonLive TelemetryReal-time GPS tracking and performance data</pivAI Analysis</hAdvanced racing intelligence and coachingMobile ReadyRacing companion with offline sync);
-};
 
 const App = () => {
-  return (} /></BrowserRouter
+  return (RaceSenseProfessional Racing PlatformStart Racing</button
 };
 
 export default App;
